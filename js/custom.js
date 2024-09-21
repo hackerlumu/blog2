e60d9499c0aac5d846ff96fedfcb21415030e817
hackerlumu <56408838+hackerlumu@users.noreply.github.com>
2024-09-21 22:05:26 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/*!
+ * Item: 
+ * Description: 
+ * Author/Developer: Lumu
+ * Author/Developer URL: 
+ * Version: v2.0.0
+ * License:
+ */
+!function(n){"use strict";n((function(){})),n(window).on("load",(function(){}))}(jQuery);
